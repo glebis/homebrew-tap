ef7b1b84ab93caaa163980313c98b9afe649e74e
Gleb Kalinin <glebis@gmail.com>
2026-06-17 23:07:14 +0200

    Add cull cask (sha256 :no_check until v0.2.1 release DMGs are published)

---
+++ Casks/cull.rb
@@ -0,0 +1,28 @@
+cask "cull" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.2.1"
+  sha256 :no_check
+
+  url "https://github.com/glebis/cull/releases/download/v#{version}/Cull_#{version}_#{arch}.dmg"
+  name "Cull"
+  desc "Local-first image review and curation tool for large image sets"
+  homepage "https://github.com/glebis/cull"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "Cull.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.glebkalinin.cull",
+    "~/Library/Caches/com.glebkalinin.cull",
+    "~/Library/Preferences/com.glebkalinin.cull.plist",
+    "~/Library/Saved Application State/com.glebkalinin.cull.savedState",
+    "~/Library/WebKit/com.glebkalinin.cull",
+  ]
+end
